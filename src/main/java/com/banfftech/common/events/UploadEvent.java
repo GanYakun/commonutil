d36ade7f7817302c9ac2eb8d0f26ab98842de9cf
SunCanyang <try028@163.com>
2023-05-18 10:38:52 +0800

    上传文件

---
+++ src/main/java/com/banfftech/common/events/UploadEvent.java
@@ -0,0 +1,46 @@
+package com.banfftech.common.events;
+
+import com.dpbird.odata.Util;
+import org.apache.ofbiz.base.util.UtilHttp;
+import org.apache.ofbiz.base.util.UtilMisc;
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.service.GenericServiceException;
+import org.apache.ofbiz.service.LocalDispatcher;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author scy
+ * @date 2023/5/18
+ */
+public class UploadEvent {
+
+    public static String uploadFile(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            LocalDispatcher dispatcher = (LocalDispatcher) request.getAttribute("dispatcher");
+            Delegator delegator = (Delegator) request.getAttribute("delegator");
+            GenericValue userLogin = (GenericValue) request.getAttribute("userLogin");
+            //获取文件表单全部内容
+            Map<String, Object> multiPartMap = UtilHttp.getMultiPartParameterMap(request);
+            //关联实体
+            List<String> relations =  Arrays.asList(request.getParameter("relation").split("/"));
+            Map<String, Object> keyMap = Util.odataIdToMap(delegator, relations.get(0), request.getParameter("key"));
+            //中间表的ContentType 可以为空
+            String contentTypeId = request.getParameter("relContentTypeId");
+            dispatcher.runSync("banfftech.uploadFile", UtilMisc.toMap("multiFrom", multiPartMap, "key", keyMap,
+                    "relContentType", contentTypeId, "relation", relations, "userLogin", userLogin));
+        } catch (GenericServiceException e) {
+            e.printStackTrace();
+            response.setStatus(500);
+            return "error";
+        }
+        response.setStatus(200);
+        return "success";
+    }
+
+}
--- src/main/java/com/banfftech/common/services/CommonServices.java
+++ src/main/java/com/banfftech/common/services/CommonServices.java
@@ -2,15 +2,20 @@ package com.banfftech.common.services;
 
 import com.banfftech.common.util.CommonUtils;
 import com.dpbird.odata.OfbizODataException;
-import org.apache.ofbiz.base.util.UtilMisc;
+import com.dpbird.odata.Util;
+import org.apache.ofbiz.base.util.*;
 import org.apache.ofbiz.entity.Delegator;
 import org.apache.ofbiz.entity.GenericEntityException;
 import org.apache.ofbiz.entity.GenericValue;
-import org.apache.ofbiz.service.DispatchContext;
-import org.apache.ofbiz.service.GeneralServiceException;
-import org.apache.ofbiz.service.GenericServiceException;
-import org.apache.ofbiz.service.ServiceUtil;
+import org.apache.ofbiz.entity.model.ModelEntity;
+import org.apache.ofbiz.entity.model.ModelField;
+import org.apache.ofbiz.entity.util.EntityUtil;
+import org.apache.ofbiz.service.*;
 
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class CommonServices {
@@ -56,4 +61,102 @@ public class CommonServices {
 
         return ServiceUtil.returnSuccess();
     }
+
+    /**
+     * 通用的文件上传
+     */
+    public static Map<String, Object> uploadFile(DispatchContext dctx, Map<String, Object> context)
+            throws OfbizODataException {
+        Delegator delegator = dctx.getDelegator();
+        LocalDispatcher dispatcher = dctx.getDispatcher();
+        Locale locale = (Locale) context.get("locale");
+        GenericValue userLogin = (GenericValue) context.get("userLogin");
+        //文件数据
+        Map<String, Object> mediaInfo = UtilGenerics.checkMap(context.get("multiFrom"));
+        //文件
+        ByteBuffer fileBuff = (ByteBuffer) mediaInfo.get("file");
+        //文件名称
+        String fileName = (String) mediaInfo.get("_file_fileName");
+        //文件类型
+        String fileType = (String) mediaInfo.get("_file_contentType");
+        //主键
+        Map<String, Object> primaryKey = UtilGenerics.checkMap(context.get("key"));
+        List<String> relation = UtilGenerics.checkList(context.get("relation"));
+        //关联实体
+        String entityName = relation.get(0);
+        String mediaEntityName = relation.get(relation.size() - 1);
+
+        String relContentType = (String) context.get("relContentType");
+        try {
+            GenericValue media = findMedia(delegator, primaryKey, relation);
+            if (UtilValidate.isNotEmpty(media)) {
+                //存在文件 更新文件内容
+                String updateMedia = Util.getEntityActionService(null, media.getEntityName(), "update", delegator);
+                dispatcher.runSync(updateMedia, UtilMisc.toMap("userLogin", userLogin, "dataResourceId", media.getString("dataResourceId"), "imageData", fileBuff.array()));
+                //更新文件名称、类型
+                String updateResource = Util.getEntityActionService(null, "DataResource", "update", delegator);
+                dispatcher.runSync(updateResource, UtilMisc.toMap("userLogin", userLogin, "dataResourceId", media.getString("dataResourceId"),
+                        "dataResourceName", fileName, "mimeTypeId", fileType));
+                return ServiceUtil.returnSuccess();
+            }
+            // 创建文件
+            // 场景1: FacilityContent/Content/DataResource/ImageDataResource
+            String imageResourceService = Util.getEntityActionService(null, mediaEntityName, "create", delegator);
+            String resourceService = Util.getEntityActionService(null, "DataResource", "create", delegator);
+            // create DataResource
+            Map<String, Object> createResult = dispatcher.runSync(resourceService,
+                    UtilMisc.toMap("userLogin", userLogin, "mimeTypeId", fileType, "dataResourceName", fileName));
+            String dataResourceId = (String) createResult.get("dataResourceId");
+            // create ImageDataResource
+            dispatcher.runSync(imageResourceService, UtilMisc.toMap("userLogin", userLogin, "dataResourceId", dataResourceId, getByteField(delegator, mediaEntityName), fileBuff.array()));
+            //起源是Content 直接更新Content
+            if (entityName.endsWith("Content")) {
+                String contentService = Util.getEntityActionService(null, "Content", "update", delegator);
+                dispatcher.runSync(contentService, UtilMisc.toMap("userLogin", userLogin, "contentId", primaryKey.get("contentId"), "dataResourceId", dataResourceId));
+                return ServiceUtil.returnSuccess();
+            }
+            // 场景2: Facility/FacilityContent/Content/DataResource/ImageDataResource 创建Content和中间对象
+            String contentService = Util.getEntityActionService(null, "Content", "create", delegator);
+            String relContentService = Util.getEntityActionService(null, entityName + "Content", "create", delegator);
+            // create Content
+            createResult = dispatcher.runSync(contentService, UtilMisc.toMap("userLogin", userLogin, "dataResourceId", dataResourceId));
+            // create Rel Content
+            primaryKey.put("userLogin", userLogin);
+            primaryKey.put("contentId", createResult.get("contentId"));
+            primaryKey.put("fromDate", UtilDateTime.nowTimestamp());
+            primaryKey.put(Util.firstLowerCase(entityName + "ContentTypeId"), relContentType);
+            Map<String, Object> serviceParam = ServiceUtil.setServiceFields(dispatcher, relContentService, primaryKey, userLogin, null, locale);
+            dispatcher.runSync(relContentService, serviceParam);
+        } catch (GeneralException e) {
+            e.printStackTrace();
+            throw new OfbizODataException(e.getMessage());
+        }
+        return ServiceUtil.returnSuccess();
+    }
+
+    //查询上传文件传递的关系链 看存不存在图片
+    private static GenericValue findMedia(Delegator delegator, Map<String, Object> primaryKey, List<String> relation) throws GenericEntityException {
+        GenericValue genericValue = delegator.findOne(relation.get(0), primaryKey, true);
+        for (int i = 1; i < relation.size(); i++) {
+            genericValue = EntityUtil.getFirst(genericValue.getRelated(relation.get(i), null, null, true));
+            if (UtilValidate.isEmpty(genericValue)) {
+                return null;
+            }
+        }
+        return genericValue;
+    }
+
+    private static String getByteField(Delegator delegator, String mediaEntity) {
+        ModelEntity modelEntity = delegator.getModelEntity(mediaEntity);
+        Iterator<ModelField> fieldsIterator = modelEntity.getFieldsIterator();
+        while (fieldsIterator.hasNext()) {
+            ModelField next = fieldsIterator.next();
+            if ("byte-array".equals(next.getType())) {
+                return next.getName();
+            }
+        }
+        return null;
+    }
+
+
 }
