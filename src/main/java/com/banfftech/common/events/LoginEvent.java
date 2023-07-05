a61319021b2220211f907f3871f6ad7f37bccf43
SunCanyang <try028@163.com>
2023-07-05 16:00:10 +0800

    更新登录

--- src/main/java/com/banfftech/common/events/LoginEvent.java
+++ src/main/java/com/banfftech/common/events/LoginEvent.java
@@ -8,20 +8,19 @@ import org.apache.ofbiz.common.login.LoginServices;
 import org.apache.ofbiz.entity.Delegator;
 import org.apache.ofbiz.entity.GenericEntityException;
 import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.entity.condition.EntityCondition;
+import org.apache.ofbiz.entity.condition.EntityConditionList;
+import org.apache.ofbiz.entity.condition.EntityExpr;
+import org.apache.ofbiz.entity.condition.EntityOperator;
 import org.apache.ofbiz.entity.util.EntityQuery;
 import org.apache.ofbiz.entity.util.EntityUtilProperties;
-import org.apache.ofbiz.service.GenericServiceException;
 import org.apache.ofbiz.service.LocalDispatcher;
-import org.apache.ofbiz.service.ServiceUtil;
 import org.apache.ofbiz.webapp.control.LoginWorker;
-import org.apache.ofbiz.webapp.stats.VisitHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Locale;
 import java.util.Map;
-
-import static org.apache.ofbiz.base.util.UtilGenerics.checkMap;
 
 /**
  * @author scy
@@ -31,29 +30,18 @@ public class LoginEvent {
     private static final String MODULE = LoginEvent.class.getName();
     private static final String resource = "SecurityextUiLabels";
 
+    /**
+     * 使用externalId登录
+     */
     public static String externalLogin(HttpServletRequest request, HttpServletResponse response) {
         Delegator delegator = (Delegator) request.getAttribute("delegator");
-        LocalDispatcher dispatcher = (LocalDispatcher) request.getAttribute("dispatcher");
-        Locale locale = request.getLocale();
         //externalId
-        String externalId = request.getParameter("USERNAME");
-        String password = request.getParameter("PASSWORD");
-        if (UtilValidate.isNotEmpty(request.getAttribute("USERNAME"))) {
-            externalId = (String) request.getAttribute("USERNAME");
-        }
-        if (UtilValidate.isNotEmpty(request.getAttribute("PASSWORD"))) {
-            password = (String) request.getAttribute("PASSWORD");
-        }
-        if (UtilValidate.isEmpty(externalId)) {
-            String message = UtilProperties.getMessage(resource, "loginevents.username_was_empty_reenter", UtilHttp.getLocale(request));
-            request.setAttribute("_ERROR_MESSAGE_", message);
+        Map<String, String> loginForm = getLoginForm(request);
+        if (UtilValidate.isEmpty(loginForm)) {
             return "error";
         }
-        if (UtilValidate.isEmpty(password)) {
-            String message = UtilProperties.getMessage(resource, "loginevents.password_was_empty_reenter", UtilHttp.getLocale(request));
-            request.setAttribute("_ERROR_MESSAGE_", message);
-            return "error";
-        }
+        String externalId = loginForm.get("username");
+        String password = loginForm.get("password");
         try {
             GenericValue party = EntityQuery.use(delegator).from("Party").where("externalId", externalId).queryFirst();
             if (UtilValidate.isEmpty(party)) {
@@ -61,38 +49,113 @@ public class LoginEvent {
                 request.setAttribute("_ERROR_MESSAGE_", message);
                 return "error";
             }
-            GenericValue userLogin = EntityQuery.use(delegator).from("UserLogin").where("partyId", party.getString("partyId")).queryFirst();
-            if (UtilValidate.isEmpty(userLogin)) {
-                String message = UtilProperties.getMessage(resource, "loginevents.username_not_found_reenter", UtilHttp.getLocale(request));
-                request.setAttribute("_ERROR_MESSAGE_", message);
-                return "error";
-            }
-            boolean useEncryption = "true".equals(EntityUtilProperties.getPropertyValue("security", "password.encrypt", delegator));
-            if (LoginServices.checkPassword(userLogin.getString("currentPassword"), useEncryption, password)) {
-                LoginWorker.doBasicLogin(userLogin, request);
-                request.setAttribute("_LOGIN_PASSED_", "TRUE");
-                return "success";
-            } else {
-                String message = UtilProperties.getMessage(resource,"loginservices.password_incorrect", locale);
-                request.setAttribute("_ERROR_MESSAGE_", message);
-                return "error";
-            }
-
-//            String visitId = VisitHandler.getVisitId(request.getSession());
-//            Map<String, Object> result = dispatcher.runSync("userLogin", UtilMisc.toMap("login.username", userLogin.getString("userLoginId"), "login.password", password,
-//                    "visitId", visitId, "locale", UtilHttp.getLocale(request), "request", request));
-//            if (ServiceUtil.isError(result)) {
-//                request.setAttribute("_ERROR_MESSAGE_", ServiceUtil.getErrorMessage(result));
-//                return "error";
-//            }
-//            GenericValue loginUser = (GenericValue) result.get("userLogin");
-//            Map<String, Object> userLoginSession = checkMap(result.get("userLoginSession"), String.class, Object.class);
-//            return LoginWorker.doMainLogin(request, response, loginUser, userLoginSession);
+            return doUserLogin(request, party.getString("partyId"), password);
         } catch (GenericEntityException e) {
             request.setAttribute("_ERROR_MESSAGE_", e.getMessage());
             return "error";
         }
-
-
     }
+
+    /**
+     * 使用手机号登录
+     */
+    public static String telLogin(HttpServletRequest request, HttpServletResponse response) {
+        Delegator delegator = (Delegator) request.getAttribute("delegator");
+        Map<String, String> loginForm = getLoginForm(request);
+        if (UtilValidate.isEmpty(loginForm)) {
+            return "error";
+        }
+        //tel
+        String tel = loginForm.get("username");
+        String password = loginForm.get("password");
+        try {
+            GenericValue partyAndContact = EntityQuery.use(delegator).from("PartyAndContact").where("phoneMobile", tel).queryFirst();
+            if (UtilValidate.isEmpty(partyAndContact)) {
+                String message = UtilProperties.getMessage(resource, "loginevents.username_not_found_reenter", UtilHttp.getLocale(request));
+                request.setAttribute("_ERROR_MESSAGE_", message);
+                return "error";
+            }
+            return doUserLogin(request, partyAndContact.getString("partyId"), password);
+        } catch (GenericEntityException e) {
+            request.setAttribute("_ERROR_MESSAGE_", e.getMessage());
+            return "error";
+        }
+    }
+
+    /**
+     * 手机号和external都可以登录
+     */
+    public static String externalAndTelLogin(HttpServletRequest request, HttpServletResponse response) {
+        Delegator delegator = (Delegator) request.getAttribute("delegator");
+        Map<String, String> loginForm = getLoginForm(request);
+        if (UtilValidate.isEmpty(loginForm)) {
+            return "error";
+        }
+        String username = loginForm.get("username");
+        String password = loginForm.get("password");
+        try {
+            EntityCondition findCondition = EntityCondition.makeCondition(UtilMisc.toList(EntityCondition.makeCondition("externalId", username),
+                    EntityCondition.makeCondition("phoneMobile", username)), EntityOperator.OR);
+            GenericValue partyAndContact = EntityQuery.use(delegator).from("PartyAndContact").where(findCondition).queryFirst();
+            if (UtilValidate.isEmpty(partyAndContact)) {
+                String message = UtilProperties.getMessage(resource, "loginevents.username_not_found_reenter", UtilHttp.getLocale(request));
+                request.setAttribute("_ERROR_MESSAGE_", message);
+                return "error";
+            }
+            return doUserLogin(request, partyAndContact.getString("partyId"), password);
+        } catch (GenericEntityException e) {
+            request.setAttribute("_ERROR_MESSAGE_", e.getMessage());
+            return "error";
+        }
+    }
+
+    /**
+     * 登录
+     */
+    private static String doUserLogin(HttpServletRequest request, String partyId, String password) throws GenericEntityException {
+        Delegator delegator = (Delegator) request.getAttribute("delegator");
+        GenericValue userLogin = EntityQuery.use(delegator).from("UserLogin").where("partyId", partyId).queryFirst();
+        if (UtilValidate.isEmpty(userLogin)) {
+            String message = UtilProperties.getMessage(resource, "loginevents.username_not_found_reenter", UtilHttp.getLocale(request));
+            request.setAttribute("_ERROR_MESSAGE_", message);
+            return "error";
+        }
+        boolean useEncryption = "true".equals(EntityUtilProperties.getPropertyValue("security", "password.encrypt", delegator));
+        if (LoginServices.checkPassword(userLogin.getString("currentPassword"), useEncryption, password)) {
+            LoginWorker.doBasicLogin(userLogin, request);
+            request.setAttribute("_LOGIN_PASSED_", "TRUE");
+            return "success";
+        } else {
+            String message = UtilProperties.getMessage(resource, "loginservices.password_incorrect", request.getLocale());
+            request.setAttribute("_ERROR_MESSAGE_", message);
+            return "error";
+        }
+    }
+
+    /**
+     * 从请求数据中获取用户名称和密码
+     */
+    private static Map<String, String> getLoginForm(HttpServletRequest request) {
+        String username = request.getParameter("USERNAME");
+        String password = request.getParameter("PASSWORD");
+        if (UtilValidate.isNotEmpty(request.getAttribute("USERNAME"))) {
+            username = (String) request.getAttribute("USERNAME");
+        }
+        if (UtilValidate.isNotEmpty(request.getAttribute("PASSWORD"))) {
+            password = (String) request.getAttribute("PASSWORD");
+        }
+        if (UtilValidate.isEmpty(username)) {
+            String message = UtilProperties.getMessage(resource, "loginevents.username_was_empty_reenter", UtilHttp.getLocale(request));
+            request.setAttribute("_ERROR_MESSAGE_", message);
+            return null;
+        }
+        if (UtilValidate.isEmpty(password)) {
+            String message = UtilProperties.getMessage(resource, "loginevents.password_was_empty_reenter", UtilHttp.getLocale(request));
+            request.setAttribute("_ERROR_MESSAGE_", message);
+            return null;
+        }
+        return UtilMisc.toMap("username", username, "password", password);
+    }
+
+
 }
